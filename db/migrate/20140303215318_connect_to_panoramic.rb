c012c008d3ddba96df2a62d55f0b9ecdd50cc451
Bryan K <bkahler3@gmail.com>
2014-03-03 13:54:13 -0800

    Connects to Panoramic psql db

---
+++ db/migrate/20140303215318_connect_to_panoramic.rb
@@ -0,0 +1,8 @@
+class ConnectToPanoramic < ActiveRecord::Migration
+  ActiveRecord::Schema.define(version: 0) do
+
+    # These are extensions that must be enabled in order to support this database
+    enable_extension "plpgsql"
+
+  end
+end
